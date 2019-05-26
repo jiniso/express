9ff8006ebecd22805b409f4229d510cd8182556f
jiniso <joseph.jager@gmail.com>
2019-05-26 14:58:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const templateRoot = path.join(__dirname, '/templates');
 
 // add some security-related headers to the response
 app.use(helmet());
-app.get('*', (req, res) => {
+app.get('*', (req, res) => { 
     res.set('Content-Type', 'text/html');
     //let password = process.env.DATABASE_PASSWORD; 
     //res.send(200, `<h1>Secret: ${password}</h1>`); 
