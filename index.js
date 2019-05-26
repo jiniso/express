5f988cb403f6d3d1e5280885284bdd25cf3d9f65
jiniso <joseph.jager@gmail.com>
2019-05-25 21:35:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 const app = express();
-//const YAML = require('yamljs');
-//const swaggerUi = require('swagger-ui-express');
-
 const templateRoot = path.join(__dirname, '/templates');
-//const swaggerDocument = YAML.load('./swagger.yml');
 
 // add some security-related headers to the response
 app.use(helmet());
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html');
-    let password = process.env.DATABASE_PASSWORD; 
+    //let password = process.env.DATABASE_PASSWORD; 
+    //res.send(200, `<h1>Secret: ${password}</h1>`); 
 
     res.sendFile(path.join(__dirname+'/index.html')); 
-    //res.send(200, `<h1>Secret: ${password}</h1>`); 
 })
 
 module.exports = app
