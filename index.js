1b933039259edaa3b2bd029831088e1125d0d89e
jiniso <joseph.jager@gmail.com>
2019-05-25 20:51:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html');
     let password = process.env.DATABASE_PASSWORD; 
 
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/index.html')); 
     //res.send(200, `
     //    <h1>Secret: ${password}</h1>   
     ///`); 
