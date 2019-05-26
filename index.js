5d522561eccbcf48dbd93d8ab08b4f3c2789e0fc
= <=>
2019-05-25 17:41:04 -0700

    static file test

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const helmet = require('helmet')
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const app = express();
 
-const app = express()
+const templateRoot = path.join(__dirname, '/templates');
 
 // add some security-related headers to the response
 app.use(helmet())
 
 app.get('*', (req, res) => {
-    res.set('Content-Type', 'text/html')
+    res.set('Content-Type', 'text/html');
+
+    res.sendFile(path.join(__dirname+'/index.html'));
+ 
+    /*
     res.status(200).send(`
         <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
         <h2>Go to <a href="/about">/about</a></h2>
     `)
+    */
 })
 
 module.exports = app
