5c4652f04be8be92a35626b1c697743d301a4206
= <=>
2019-05-25 19:03:55 -0700

    testing swagger

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 const app = express();
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 
 const templateRoot = path.join(__dirname, '/templates');
 
 // add some security-related headers to the response
-app.use(helmet())
+app.use(helmet());
 
-app.get('*', (req, res) => {
+/*
+app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
 
     res.sendFile(path.join(__dirname+'/index.html'));
  
-    /*
-    res.status(200).send(`
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
-        <h2>Go to <a href="/about">/about</a></h2>
-    `)
-    */
+    //res.status(200).send(`
+    //    <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
+    //    <h2>Go to <a href="/about">/about</a></h2>
+    //`)     
 })
+*/
+
+const swaggerDocument = YAML.load('./swagger.yml');
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app
